308ffde0cdd52b269890ae33240e0b2f8b9ed2db
AstridBatres-Guerrero <93022962+AstridBatres@users.noreply.github.com>
2022-05-14 11:42:24 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Navbar from "./components/navbar";
+import Footer from "./components/footer";
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+      <h1>Welcome to my store!</h1>
+
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
